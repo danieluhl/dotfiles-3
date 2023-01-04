b7cc9787b9a2e5a9e1fa7c719628f1c8c25079ac
Dan Uhl <duhl@wayfair.com>
2023-01-04 15:49:29 -0500

    removeds harpoon

--- public/config/nvim/lua/duhl/gset.lua
+++ public/config/nvim/lua/duhl/gset.lua
@@ -14,14 +14,4 @@ vim.cmd([[
 
 	" dont add mappings from context for <S-h>
 	let g:context_add_mappings = 0
-
-
-	augroup ftplugin
-		au!
-		au BufWinEnter * set formatoptions-=cro
-		au BufNewFile,BufRead *.json setl filetype=jsonc " To allow comments on json files
-		au FileType man setl laststatus=0 noruler
-		au FileType vim,html,css,json,javascript,javascriptreact,typescript,typescriptreact,lua,sh,zsh setl sw=2
-		au TermOpen term://* setl nornu nonu nocul so=0 scl=no
-	augroup END
 ]])
