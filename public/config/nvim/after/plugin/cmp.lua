fe31bdaed5d284fb4b570162b1bee071fc74dd2b
Dan Uhl <duhl@wayfair.com>
2023-03-01 12:48:55 -0500

    remove autocomplete tab mapping

--- public/config/nvim/after/plugin/cmp.lua
+++ public/config/nvim/after/plugin/cmp.lua
@@ -28,37 +28,43 @@ cmp.setup({
 		-- ["<C-e>"] = cmp.mapping.complete(),
 		-- ["<C-e>"] = luasnip.expand(),
 
-		-- ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		-- ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently
+		-- selected item. Set `select` to `false` to only confirm explicitly
+		-- selected items.
 		["<CR>"] = cmp.mapping.confirm({
 			-- behavior = cmp.ConfirmBehavior.Replace,
 			select = true,
 		}),
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expandable() then
-				luasnip.expand()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			else
-				fallback()
-			end
-		end, {
-			"i",
-			"s",
+		["<Tab>"] = cmp.mapping.confirm({
+			-- behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
 		}),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, {
-			"i",
-			"s",
-		}),
+		-- ["<Tab>"] = cmp.mapping(function(fallback)
+		-- 	if cmp.visible() then
+		-- 		cmp.select_next_item()
+		-- 	elseif luasnip.expandable() then
+		-- 		luasnip.expand()
+		-- 	elseif luasnip.expand_or_jumpable() then
+		-- 		luasnip.expand_or_jump()
+		-- 	else
+		-- 		fallback()
+		-- 	end
+		-- end, {
+		-- 	"i",
+		-- 	"s",
+		-- }),
+		-- ["<S-Tab>"] = cmp.mapping(function(fallback)
+		-- 	if cmp.visible() then
+		-- 		cmp.select_prev_item()
+		-- 	elseif luasnip.jumpable(-1) then
+		-- 		luasnip.jump(-1)
+		-- 	else
+		-- 		fallback()
+		-- 	end
+		-- end, {
+		-- 	"i",
+		-- 	"s",
+		-- }),
 	},
 
 	formatting = {
