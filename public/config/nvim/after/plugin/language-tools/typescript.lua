e8bdf81aee6413e1b3ab1d862c7ac98097d4f162
Dan Uhl <dan.uhl@posh.tech>
2023-12-01 06:39:30 -0500

    adds ocaml opam config

--- public/config/nvim/after/plugin/language-tools/typescript.lua
+++ public/config/nvim/after/plugin/language-tools/typescript.lua
@@ -1,8 +1,7 @@
-local nullLs = require("null-ls")
 require("typescript").setup({
   disable_commands = false, -- prevent the plugin from creating Vim commands
-  debug = false, -- enable debug logging for commands
+  debug = false,           -- enable debug logging for commands
   go_to_source_definition = {
-    fallback = true, -- fall back to standard LSP definition on failure
+    fallback = true,       -- fall back to standard LSP definition on failure
   },
 })
