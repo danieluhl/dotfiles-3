cfc9028b2bdfabbc44f9627d1f757bdc8114f356
Dan Uhl <dan.uhl@posh.tech>
2024-01-25 07:26:43 -0500

    adds raycast scripts

---
+++ public/raycast_scripts/open_warp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title open_arc
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author typing_turtle
+# @raycast.authorURL https://raycast.com/typing_turtle
+
+open -a Warp
+
