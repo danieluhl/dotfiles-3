87e1b3900261e203c1e36c9fbd29e31ef9d4ea32
Dan Uhl <dan.uhl@posh.tech>
2024-02-13 04:51:25 -0500

    switch from rtx to mise and combine lsp configs

--- public/config/nvim/after/plugin/lsp.lua
+++ public/config/nvim/after/plugin/lsp.lua
@@ -1,54 +1,63 @@
--- lsp-zero does the wiring of lspconfig
 local lsp_zero = require("lsp-zero")
 local lspconfig = require("lspconfig")
+local cmp = require("cmp")
 
+lsp_zero.extend_lspconfig()
 lsp_zero.preset("recommended")
 
--- Fix Undefined global 'vim'
-lsp_zero.configure("lua_ls", {
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { "vim", "actions" },
-			},
-		},
-	},
-})
-
 lsp_zero.on_attach(function(client, bufnr)
-	-- see :help lsp-zero-keybindings
-	-- to learn the available actions
 	lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
-lsp_zero.set_preferences({
-	suggest_lsp_servers = false,
-	set_lsp_keymaps = false,
-	sign_icons = {
-		error = "E",
-		warn = "W",
-		hint = "H",
-		info = "I",
+-- diagnostics global defaults
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+	underline = true,
+	virtual_text = true,
+	signs = true,
+})
+
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+cmp.setup({
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
 	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+		["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+		["<Tab>"] = nil,
+		["<S-Tab>"] = nil,
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
+		["<C-Space>"] = cmp.mapping.complete(),
+	}),
 })
 
-lsp_zero.setup()
-
--- SETUP SERVERS
-lspconfig.tsserver.setup({
-	on_attach = function(client, bufnr)
-		client.resolved_capabilities.document_formatting = false
-		on_attach(client, bufnr)
-	end,
-})
--- other servers that don't need config
-lsp_zero.setup_servers({ "lua_ls", "rust_analyzer", "astro" })
-
-require('mason').setup({})
-require('mason-lspconfig').setup({
-	ensure_installed = {},
+require("mason").setup()
+require("mason-lspconfig").setup({
 	handlers = {
 		lsp_zero.default_setup,
+
+		lua_ls = function()
+			lspconfig.lua_ls.setup({
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim", "it", "describe", "before_each", "after_each" },
+						},
+					},
+				},
+			})
+		end,
+
+		tsserver = function()
+			lspconfig.tsserver.setup({
+				on_attach = function(client)
+					client.server_capabilities.documentFormattingProvider = false
+				end,
+				single_file_support = false,
+			})
+		end,
 	},
 })
 
