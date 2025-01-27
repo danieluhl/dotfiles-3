35ea1762a6a660ec7c6f1b3868551d32a0bb3242
Dan Uhl <dan.uhl@posh.tech>
2025-01-26 20:37:08 -0500

    use enter to accept completions

--- public/config/nvim/lua/duhl/lazy/completion.lua
+++ public/config/nvim/lua/duhl/lazy/completion.lua
@@ -19,7 +19,7 @@ return {
       -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
       -- see the "default configuration" section below for full documentation on how to define
       -- your own keymap.
-      keymap = { preset = "default" },
+      keymap = { preset = "default", ["<cr>"] = { "accept", "fallback" } },
 
       appearance = {
         -- Sets the fallback highlight groups to nvim-cmp's highlight groups
@@ -36,7 +36,7 @@ return {
       sources = {
         default = { "lsp", "path", "snippets", "buffer" },
         -- optionally disable cmdline completions
-        -- cmdline = {},
+        cmdline = {},
       },
 
       -- experimental signature help support
