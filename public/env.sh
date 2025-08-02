7398acc56f8fa30e6206f583aee745959e74d449
Dan Uhl <danielruhl@gmail.com>
2025-08-02 06:17:35 -0400

    moves all path variable setting to env.sh

--- public/env.sh
+++ public/env.sh
@@ -1,39 +1,48 @@
 #!/bin/zsh
 
-# to find a homebrew package to get the path use `brew info [package]`
-
-# User configuration
-export PATH=$PATH:/usr/local/share/npm/bin
-export PATH=$PATH:/usr/local/lib/node_modules
-export PATH=$PATH:~/Library/Python/3.8/bin
-export PATH=$PATH:/usr/local/lib/node_modules
-export PATH=$PATH:~/bin
-
-# rustlang bin to path
-export RUSTPATH=~/.cargo
-export PATH=$PATH:$RUSTPATH/bin
-
-# To install rust analyzer and get it in PATH:
-# > rustup component add rust-analyzer
-# > ln -s ~/.rustup/toolchains/stable-aarch64-apple-darwin/bin/rust-analyzer ~/.cargo/rust-analyzer
-
-export PATH=$PATH:$RUSTPATH/bin
-
-# Add golang bin to PATH
-export GOPATH=~/go
-export PATH=$PATH:$GOPATH/bin
-
 # package managers
-export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
-export PNPM_HOME="/Users/duhl/Library/pnpm"
-export PATH="$PNPM_HOME:$PATH"
-
-# Top priority paths go at the start of path
-export PATH="/usr/local/bin:$PATH"
-
-# Homebrew packages
-export PATH="/opt/homebrew/opt/ruby/bin:/opt/homebrew/bin:$PATH"
-export PATH="/opt/homebrew/sbin:$PATH"
+export PNPM_HOME="$HOME/pnpm"
 
 # Set editor to vim
 export EDITOR="nvim"
+# Number of directories to show in the window title
+export ZSH_WINDOW_TITLE_DIRECTORY_DEPTH=2
+
+# rust
+export RUSTPATH="$HOME/.cargo"
+# golang
+export GOPATH="#HOME/go"
+# bun
+export BUN_INSTALL="$HOME/.bun"
+
+PATH_DIRS=(
+  # Homebrew packages
+  "/opt/homebrew/opt/ruby/bin:/opt/homebrew/bin"
+  "/opt/homebrew/sbin"
+  "$PNPM_HOME"
+  "$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin"
+  "$RUSTPATH"
+  "$GOPATH"
+  "$BUN_INSTALL/bin"
+  "$HOME/bin"
+  "/usr/local/bin"
+)
+
+# Iterate over each directory in the list
+for dir in "${PATH_DIRS[@]}"; do
+  # Use the same safe check as before to see if the directory is already in the PATH
+  # The ":$PATH:" trick handles edge cases.
+  case ":$PATH:" in
+    *":$dir:"*)
+      # If the directory is found, do nothing.
+      ;;
+    *)
+      # If the directory is not found, prepend it to the PATH.
+      export PATH="$dir:$PATH"
+      ;;
+  esac
+done
+
+# Adds deno env variables to path
+. "/Users/danieluhl/.deno/env"
+
