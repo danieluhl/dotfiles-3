280dd5e47693bbcc23daf7a689d7e0a61f23c733
Dan Uhl <danielruhl@gmail.com>
2025-09-25 17:00:52 -0400

    adds elixir support

--- public/config/nvim/lua/duhl/lazy/treesitter.lua
+++ public/config/nvim/lua/duhl/lazy/treesitter.lua
@@ -1,36 +1,38 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"tsx",
-				"typescript",
-				"javascript",
-				"markdown",
-				"lua",
-				"rust",
-				"toml",
-				"go",
-				"ocaml",
-				"astro",
-				"gleam",
-			},
-			highlight = {
-				enable = {
-					"tsx",
-					"typescript",
-					"javascript",
-					"markdown",
-					"lua",
-					"rust",
-					"toml",
-					"go",
-					"ocaml",
-					"astro",
-					"gleam",
-				},
-			},
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "tsx",
+        "typescript",
+        "javascript",
+        "markdown",
+        "lua",
+        "rust",
+        "toml",
+        "go",
+        "ocaml",
+        "astro",
+        "gleam",
+        "elixir",
+      },
+      highlight = {
+        enable = {
+          "tsx",
+          "typescript",
+          "javascript",
+          "markdown",
+          "lua",
+          "rust",
+          "toml",
+          "go",
+          "ocaml",
+          "astro",
+          "gleam",
+          "elixir",
+        },
+      },
+    })
+  end,
 }
