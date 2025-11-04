65f58ad8f242753e1be826f5509a3a4283c6e2c1
Dan Uhl <danielruhl@gmail.com>
2025-11-04 12:43:42 -0500

    comments out autopairs (again)

--- public/config/nvim/lua/duhl/lazy/autopairs.lua
+++ public/config/nvim/lua/duhl/lazy/autopairs.lua
@@ -1,7 +1,8 @@
 return {
-  "windwp/nvim-autopairs",
-  event = "InsertEnter",
-  config = true,
+  -- REMINDER: this is painful because there are many cases when you just want an open paren
+  -- "windwp/nvim-autopairs",
+  -- event = "InsertEnter",
+  -- config = true,
   -- use opts = {} for passing setup options
   -- this is equivalent to setup({}) function
 }
