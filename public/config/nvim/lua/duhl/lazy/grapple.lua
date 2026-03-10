3edd37adb9053226b459fcb3284e23a7f653b9b6
Dan Uhl <dan@anedot.com>
2026-03-10 08:59:23 -0400

    adds grapple

---
+++ public/config/nvim/lua/duhl/lazy/grapple.lua
@@ -0,0 +1,54 @@
+return {
+  "cbochs/grapple.nvim",
+  -- Plugin dependencies required for grapple.nvim to function properly
+  dependencies = {
+    { "nvim-tree/nvim-web-devicons", lazy = true }
+  },
+  opts = {
+    scope = "package_json",
+
+    scopes = {
+      {
+        name = "package_json",
+        desc = "Scope defined by nearest package.json",
+        fallback = "git",
+        cache = {
+          event = { "BufEnter", "FocusGained" },
+          debounce = 1000,
+        },
+        resolver = function()
+          local package_files = vim.fs.find("package.json", {
+            upward = true,
+            stop = vim.loop.os_homedir(),
+          })
+
+          if #package_files == 0 then
+            return
+          end
+
+          local root = vim.fn.fnamemodify(package_files[1], ":h")
+          local id = root
+          local path = root
+
+          return id, path
+        end,
+      },
+    },
+  },
+  event = { "BufReadPost", "BufNewFile" },
+  cmd = "Grapple",
+  keys = {
+    { "'a", "<cmd>Grapple tag<cr>",            desc = "Grapple toggle tag" },
+    { "'t", "<cmd>Grapple toggle_tags<cr>",    desc = "Grapple open tags window" },
+
+    { "'s", "<cmd>Grapple select index=1<cr>", desc = "Select first tag" },
+    { "'d", "<cmd>Grapple select index=2<cr>", desc = "Select second tag" },
+    { "'f", "<cmd>Grapple select index=3<cr>", desc = "Select third tag" },
+    { "'g", "<cmd>Grapple select index=4<cr>", desc = "Select fourth tag" },
+    -- index out of bounds... seems like I can only have 4 tags?
+    { "'w", "<cmd>Grapple select index=5<cr>", desc = "Select fifth tag" },
+    { "'e", "<cmd>Grapple select index=6<cr>", desc = "Select sixth tag" },
+    -- { "<leader>n", "<cmd>Grapple cycle_tags next<cr>", desc = "Grapple cycle next tag" },
+    -- { "<leader>p", "<cmd>Grapple cycle_tags prev<cr>", desc = "Grapple cycle previous tag" },
+  },
+}
--- public/config/nvim/lua/duhl/lazy/harpoon.lua
+++ public/config/nvim/lua/duhl/lazy/harpoon.lua
@@ -1,39 +1,39 @@
 return {
-  "ThePrimeagen/harpoon",
-  branch = "harpoon2",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    -- require("telescope").load_extension("harpoon")
-    local harpoon = require("harpoon")
+  -- "ThePrimeagen/harpoon",
+  -- branch = "harpoon2",
+  -- dependencies = { "nvim-lua/plenary.nvim" },
+  -- config = function()
+  --   -- require("telescope").load_extension("harpoon")
+  --   local harpoon = require("harpoon")
 
-    -- -- REQUIRED
-    harpoon:setup()
-    -- -- REQUIRED
+  --   -- -- REQUIRED
+  --   harpoon:setup()
+  --   -- -- REQUIRED
 
-    vim.keymap.set("n", "'a", function()
-      harpoon:list():add()
-    end)
-    vim.keymap.set("n", "'t", function()
-      harpoon.ui:toggle_quick_menu(harpoon:list())
-    end)
+  --   vim.keymap.set("n", "'a", function()
+  --     harpoon:list():add()
+  --   end)
+  --   vim.keymap.set("n", "'t", function()
+  --     harpoon.ui:toggle_quick_menu(harpoon:list())
+  --   end)
 
-    vim.keymap.set("n", "'s", function()
-      harpoon:list():select(1)
-    end)
-    vim.keymap.set("n", "'d", function()
-      harpoon:list():select(2)
-    end)
-    vim.keymap.set("n", "'f", function()
-      harpoon:list():select(3)
-    end)
-    vim.keymap.set("n", "'g", function()
-      harpoon:list():select(4)
-    end)
-    vim.keymap.set("n", "'w", function()
-      harpoon:list():select(5)
-    end)
-    vim.keymap.set("n", "'e", function()
-      harpoon:list():select(6)
-    end)
-  end,
+  --   vim.keymap.set("n", "'s", function()
+  --     harpoon:list():select(1)
+  --   end)
+  --   vim.keymap.set("n", "'d", function()
+  --     harpoon:list():select(2)
+  --   end)
+  --   vim.keymap.set("n", "'f", function()
+  --     harpoon:list():select(3)
+  --   end)
+  --   vim.keymap.set("n", "'g", function()
+  --     harpoon:list():select(4)
+  --   end)
+  --   vim.keymap.set("n", "'w", function()
+  --     harpoon:list():select(5)
+  --   end)
+  --   vim.keymap.set("n", "'e", function()
+  --     harpoon:list():select(6)
+  --   end)
+  -- end,
 }
