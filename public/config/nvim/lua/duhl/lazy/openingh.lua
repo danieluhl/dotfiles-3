d89b8b5c82eba231b74d3d28566fa04521a9c063
Dan Uhl <dan@anedot.com>
2026-03-19 12:00:38 -0400

    fixes formatting and diff tool

--- public/config/nvim/lua/duhl/lazy/openingh.lua
+++ public/config/nvim/lua/duhl/lazy/openingh.lua
@@ -2,7 +2,7 @@ return {
   "almo7aya/openingh.nvim",
   config = function()
     -- for repository page
-    vim.api.nvim_set_keymap("n", "<Leader>gr", ":OpenInGHRepo <CR>", { silent = true, noremap = true })
+    -- vim.api.nvim_set_keymap("n", "<Leader>gr", ":OpenInGHRepo <CR>", { silent = true, noremap = true })
 
     -- for current file page
     vim.api.nvim_set_keymap("n", "<Leader>gf", ":OpenInGHFile <CR>", { silent = true, noremap = true })
